462482b31f810bc63cf29551bb6d0b1b42c147e1
almarrab <ffaasffaas@gmail.com>
2025-07-13 17:35:36 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,1 @@
+import React from 'react'; import Login from './components/Login'; const App = () => <Login />; export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,1 @@
+import React from 'react'; const Dashboard = () => <div><h2>Dashboard Page</h2></div>; export default Dashboard;
